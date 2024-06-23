6a5ad417573a9a54fd5c783d9b8be593217b1fcc
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-23 19:45:08 +0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+let product = [
+    {
+        brand : "Infinix Hot 40i",
+        price : "27,099",
+        retail_price : "28,499",
+        image : "./assest/Image/infinix-hot-40i-pakistan.webp",
+        discount : <img src="assest/Image/voucher.svg" height="30" width="57" alt="voucher"></img>
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
