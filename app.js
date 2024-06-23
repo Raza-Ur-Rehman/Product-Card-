e718c02d9ca14d24370a4cf649e096eb56ad1872
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-23 20:06:05 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
+let productList = document.querySelector("product-list");
+
+
 let product = [
     {
-        brand : "Infinix Hot 40i",
-        price : "27,099",
-        retail_price : "28,499",
-        image : "./assest/Image/infinix-hot-40i-pakistan.webp",
-        discount : <img src="assest/Image/voucher.svg" height="30" width="57" alt="voucher"></img>
+        "brand" : "Infinix Hot 40i",
+        "price" : "27,099",
+        "retail_price" : "28,499",
+        "image" : "./assest/Image/infinix-hot-40i-pakistan.webp",
+        "discount" :
     }
+
 ]
+for(let key in product) {
+    productList.innerHTML = 
+    `<div class="product-box">
+                        <a href="#">
+                            <div class="img-box">
+                                <img src="${}" alt="">
+                            </div>
+                            <div class="detail-box">
+                                <div class="title">${Object.keys(product)}</div>
+                                <div class="price-box">
+                                    <sup>Rs</sup>
+                                    27,099
+                                </div>
+                                <div class="price-diff">
+                                    <div class="price-diff-retail">
+                                        <sup>Rs</sup>
+                                        28,499
+                                    </div>
+                                    <div class="discount">
+                                        <img src="assest/Image/voucher.svg" height="30" width="57" alt="voucher">
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>`
+
+}
 
 
 
